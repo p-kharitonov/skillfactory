35ca8d89381372c87d6d06bc886a7a1e257e9870
Pavel <workingpavel@mail.ru>
2021-03-01 20:45:51 +0300

    1.10.1
    1.10.1

---
+++ 1.10.1 - figure/figure.py
@@ -0,0 +1,11 @@
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    def __str__(self):
+        return f'x: {self.x}\ny: {self.y}\nwidth: {self.width}\nheight: {self.height}'
+
+figure = Rectangle (5, 10, 50, 100)
+print(figure)
