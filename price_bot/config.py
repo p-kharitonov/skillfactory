9f1e48204755b89cb90249a0763e73691118bd2b
Pavel <workingpavel@mail.ru>
2021-04-13 12:16:13 +0300

    Create config.py

---
+++ price_bot/config.py
@@ -0,0 +1,11 @@
+TOKEN = '' # Токен Telegram
+
+TOKEN_CURRENCY = '' # Токен exchangeratesapi.io
+
+keys = {
+    'биткоин': 'BTC',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'фунт': 'GBP'
+}
